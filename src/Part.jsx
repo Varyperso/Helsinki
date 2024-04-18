7f4befd2300d2e7ddf458ee3e2489234be18d45e
Varyperso <167094617+Varyperso@users.noreply.github.com>
2024-04-18 23:16:42 +0300

    Delete src directory

--- src/Part.jsx
+++
@@ -1,7 +0,0 @@
-const Part = ({ partsAndExercises }) => {
-  let key = partsAndExercises[1].name;
-  let value = partsAndExercises[1].exercises;
-  return <div>{key + ": " + value}</div>;
-};
-
-export default Part;
